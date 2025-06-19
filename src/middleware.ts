795231e272faee4b59598b3b08e810deb06a220a
Williamho4 <willyho_@hotmail.se>
2025-06-19 18:37:56 +0200

    fixed-middleware-redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,27 @@
-import { getSessionCookie } from "better-auth/cookies";
-import { NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from 'better-auth/cookies'
+import { NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(req: NextRequest) {
-  const { nextUrl } = req;
-  const sessionCookie = getSessionCookie(req);
+  const { nextUrl } = req
+  const sessionCookie = getSessionCookie(req)
 
-  const res = NextResponse.next();
+  const res = NextResponse.next()
 
-  const isLoggedIn = !!sessionCookie;
-  const isOnProtectedRoute = nextUrl.pathname.includes("app");
-  const isOnAuthRoute = nextUrl.pathname.startsWith("/auth");
+  const isLoggedIn = !!sessionCookie
+  const isOnProtectedRoute = nextUrl.pathname.includes('app')
+  const isOnAuthRoute = nextUrl.pathname.startsWith('/auth')
 
   if (isOnProtectedRoute && !isLoggedIn) {
-    return NextResponse.redirect(new URL("/auth/sign-in", req.url));
+    return NextResponse.redirect(new URL('/auth/sign-in', req.url))
   }
 
   if (isOnAuthRoute && isLoggedIn) {
-    return NextResponse.redirect(new URL("/app", req.url));
+    return NextResponse.redirect(new URL('/application', req.url))
   }
 
-  return res;
+  return res
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-};
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+}
